7a65501bece958a8456259cf85bf7838584892ca
Johnatan Andrés Arango R <ingyotis@gmail.com>
2020-10-26 20:17:56 -0500

    se hacen los primeros templates. Sin estilos aun

---
+++ src/template/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  const view = `
+  <header class="header-main">
+    <div class="header-logo">
+      <h1><a href="/">100tifi.ko</a></h1>
+    </div>
+    <div class="header-nav">
+      <a href="#/about/">About</a>
+    </div>
+  </header>
+  `
+  return view
+}
+
+export default Header
